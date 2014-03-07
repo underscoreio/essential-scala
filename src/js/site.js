0209ec76e342fc843ce772b082aa207efd32f826
Noel Welsh <noel@untyped.com>
2014-03-07 16:32:13 +0000

    Relative paths for offline viewing
    If the user is offline rewrite paths to relative. Copied from
    
      http://stackoverflow.com/questions/7985081/how-to-deploy-a-jekyll-site-locally-with-css-js-and-background-images-included

--- src/js/site.js
+++ src/js/site.js
@@ -1,4 +1,22 @@
+var relative = null;
+if (location.protocol==='file:') {
+  relative = Array($('link[rel="canonical"]').attr('href').match(/\//g).length-2).join('../');
+  if (relative == '') relative = './';
+}
+
+function toRelative(link, index) {
+  if (!relative) return link;
+  var hash = link ? link.match(/#.*$/) : null;
+  if (hash) link = link.replace(/#.*$/, '');
+  return link?(link.replace(/^\//, relative)+(index?(link.substr(-1)=='/'?'index.html':''):'')+(hash?hash[0]:'')):null;
+}
+
 $().ready(function() {
+  if (relative) {
+    $('a').attr('href', function(a,b) {return toRelative(b, true);});
+    $('img').attr('src', function(a,b) {return toRelative(b, false);});
+  }
+
   // Transform
   //
   // <div class="solution">blah</div>
