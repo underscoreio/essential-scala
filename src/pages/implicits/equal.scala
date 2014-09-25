9c7146212ea6bcdb87ac64e134d0108187fa486b
Noel Welsh <noel@untyped.com>
2014-09-25 14:28:30 +0100

    Start on type classes

---
+++ src/pages/implicits/equal.scala
@@ -0,0 +1,15 @@
+trait Equal[A] {
+  def equal(v1: A, v2: A): Boolean
+}
+
+case class Person(name: String, email: String)
+
+object EmailEqual extends Equal[Person] {
+  def equal(v1: Person, v2: Person): Boolean =
+    v1.email == v2.email
+}
+
+object NameEmailEqual extends Equal[Person] {
+  def equal(v1: Person, v2: Person): Boolean =
+    v1.email == v2.email && v1.name == v2.name
+}
